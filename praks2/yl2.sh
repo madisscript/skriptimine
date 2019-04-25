09049fa8b9c679cbc792e2ce17e076ebcaf982a4
madis.kalasnikov@khk.ee <madisu@scriptmadis>
2019-04-25 04:29:23 -0400

    enda skripti tegemine

---
+++ praks2/yl2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#skript v2ljastab mu nime, 6ppegrupi, antud aine tunde, praeguse mulje ja edasij6oudmise
+NIMI="Madis Kalasnikov"
+OG="ISP218"
+TUNNE="aine tundub norm, 6pin uut asja jne."
+MULJE="mulje on sihuke norm, k2ib kah"
+	#seadistasin muutujad
+
+echo $NIMI; echo $OG; echo $TUNNE; echo $MULJE;
+	#v2ljastan echoga muutujad
+
+sleep 5s; clear
+	#puhastan v2ljundi peale 5 sekundit
+
+#skript l6ppeb
+
+
